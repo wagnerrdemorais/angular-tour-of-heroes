8d3bcc935db6f115bcc758b78018f0191c5781ec
wagner rodakevicz de morais <wagner.morais@objective.com.br>
2021-07-02 17:55:15 -0300

    finishing the tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -74,4 +74,16 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  searchHeroes(term: string) : Observable<Hero[]> {
+    if(!term.trim()){
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heros matching "${term}"`):
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
